6ab9c86c5b2326a496cacab97721705c39f38d4a
yongwei_ren <yongwei_ren@baofu.com>
2020-02-24 22:57:29 +0800

    角色配置

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -22,19 +22,20 @@
                     :key="item1.id">
               <!-- 渲染一级权限-->
               <el-col :span="5">
-                <el-tag>{{item1.authName}}</el-tag>
+                <el-tag closable @close="removeRightById(scope.row,item1.id)">{{item1.authName}}</el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <el-col :span="19">
                 <el-row :class="[i2 === 0 ? '' : 'bdtop','vcenter']" v-for="(item2,i2) in item1.children"
                         :key="item2.id">
                   <el-col :span="6">
-                    <el-tag type="success">{{item2.authName}}</el-tag>
+                    <el-tag type="success" closable @close="removeRightById(scope.row,item2.id)">{{item2.authName}}
+                    </el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
                   <el-col :span="18">
                     <el-tag type="warning" v-for="(item3,i3) in item2.children" :key="item3.id" closable
-                            @close="removeRightById()">
+                            @close="removeRightById(scope.row,item3.id)">
                       {{item3.authName}}
                     </el-tag>
                   </el-col>
@@ -54,8 +55,9 @@
           <template slot-scope="scope">
             <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)">编辑
             </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeRoleById(scope.row.id)">删除</el-button>
-            <el-button type="warning" icon="el-icon-setting" size="mini" @click="">分配权限</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeRoleById(scope.row.id)">删除
+            </el-button>
+            <el-button type="warning" icon="el-icon-setting" size="mini" @click="showSetRightDialog">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -93,6 +95,15 @@
         <el-button type="primary" @click="editRoleInfo()">确 定</el-button>
       </span>
     </el-dialog>
+
+    <!-- 分配权限-->
+    <el-dialog title="修改角色" :visible.sync="setRightDialogVisible" width="50%" @close="setRightDialogClosed">
+      <span></span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRightDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="setRightDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -117,7 +128,9 @@
         editFormRules: {
           roleName: [{required: true, message: '请输入角色名称', trigger: 'blur'}],
           roleDesc: [{required: true, message: '请输入角色描述', trigger: 'blur'}],
-        }
+        },
+        setRightDialogVisible: false,
+        rightsList: [],
       }
     },
     created() {
@@ -131,7 +144,7 @@
         }
         this.roleList = res.data;
       },
-      async removeRightById() {
+      async removeRightById(role, rightId) {
         //弹框提示用户是否删除
         const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
           confirmButtonText: '确定',
@@ -142,6 +155,13 @@
         if (confirmResult !== 'confirm') {
           return info.$message.info('取消了删除!')
         }
+
+        const {data: res} = await this.$http.delete(`roles/${role.id}/rights/${rightId}`);
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除权限失败');
+        }
+        // this.getRolesList();
+        role.children = res.data;
       },
       addDialogClosed() {
         this.$refs.addFormRef.resetFields();
@@ -149,7 +169,7 @@
       async addRoleInfo() {
         this.$refs.addFormRef.validate(async valid => {
           if (!valid) return;
-          const {data: res} = await this.$http.post("roles/",this.addForm);
+          const {data: res} = await this.$http.post("roles/", this.addForm);
           if (res.meta.status !== 200) {
             return this.$message.error('角色信息添加失败');
           }
@@ -185,13 +205,13 @@
           this.getRolesList();
         })
       },
-      async removeRoleById(id){
+      async removeRoleById(id) {
         const confirmResult = await this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).catch(err => err)
-        if(confirmResult !== 'confirm'){
+        if (confirmResult !== 'confirm') {
           return this.$message.info('已取消删除');
         }
         //删除用户
@@ -201,6 +221,19 @@
         }
         this.$message.success('删除角色成功!');
         this.getRolesList();
+      },
+      async showSetRightDialog() {
+        //获取权限
+        const {data: res} = await this.$http.get('rights/tree');
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除权限信息失败');
+        }
+        this.rightsList = res.data;
+        console.log(this.rightsList);
+        this.setRightDialogVisible = true;
+      },
+      setRightDialogClosed() {
+
       }
 
     }
